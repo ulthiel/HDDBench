1d1e128075567d43df913339a11c6f9aac8e4482
Ulrich Thiel <ulrich.thiel@sydney.edu.au>
2019-07-08 07:16:14 +0200

    Rewrote Mac part in HDD. Still have to deal with whitespace path

--- Readme.md
+++ Readme.md
@@ -2,8 +2,6 @@
 
 Very simple bash scripts for Linux and macOS to test computer performance. Nothing won't be super accurate but should give a rough estimate. **Use at your own risk.**
 
-I just wanted to use GNU coretools and no extra software. As the macOS versions of these tools sometimes behave differently, you first need to install the GNU coreutils via ```brew install coreutils``` if you use macOS; and for this, you first have to install [homebrew](https://brew.sh).
-
 ## HDD
 
 The script ```hdd.sh``` determines HDD write/read speed. You'll need to have 2GB of both HDD and RAM available (this can be changed in the option section of the script, however). As the script cleans up memory before the read test, you'll either have to run it as root or be able to ```sudo```.
@@ -17,9 +15,17 @@ $ sh hdd.sh
 Path for test file (e.g. /tmp): /tmp
 Running write test...
 Cleaning memory (needs sudo)...
-Password:
 Running read test...
 Cleaning up...
-HDD write speed: 47.9MB/s
-HDD read speed: 50.6MB/s
+-------------------------
+Write speed: 1501.73 MB/s
+Read speed: 2213.89 MB/s
+-------------------------
 ```
+
+### Results
+
+| HDD | Write [MB/s] | Read [MB/s] |
+| --- | ---- | ----- |
+|APPLE SSD SM0512L   | 1501.73  | 2213.89  |
+| WD My Passport 25E1, External USB  | 80.76  |  83.01 |
--- hdd.sh
+++ hdd.sh
@@ -6,9 +6,8 @@
 # You will have to enter a path for the test file to be written to. This can
 # also be passed as argument to the script.
 #
-# This script uses dd on Linux and the equivalent gdd from the coreutils on
-# macOS. The macOS native dd behaves a bit differently and has less options.
-# If you don't have gdd installed, you need to do "brew install coreutils".
+# This script uses dd. The macOS native dd has slightly different output and we
+# have to take care of this.
 #
 # Ulrich Thiel, 2019
 # ulthiel.com/math
@@ -46,16 +45,6 @@ case "${unameOut}" in
 esac
 
 ################################################################################
-# Correct dd command (use gdd from coreutils for macOS)
-################################################################################
-if [ "${machine}" = "Mac" ]
-then
-  ddcmd=gdd
-else
-  ddcmd=dd
-fi
-
-################################################################################
 # Function to clean memory
 ################################################################################
 clean_mem () {
@@ -72,25 +61,64 @@ clean_mem () {
 # Main program
 ################################################################################
 
+# on macOS dd needs lower case block size; on Linux upper case...
+if [ "${machine}" = "Mac" ]
+then
+  bs=`echo "$bs" | tr '[:upper:]' '[:lower:]'`
+elif [ "${machine}" = "Linux" ]
+then
+  bs=`echo "$bs" | tr '[:lower:]' '[:upper:]'`
+fi
+
+file="$path/.hddtstfile"
+
 # Write test
 # The conv=fdatasync seems ensures that all data is really written before dd
 # quits and reports speed
 echo "Running write test..."
-write=$(sync; $ddcmd if=/dev/zero of="$path"/.hddtstfile bs=$bs count=$count conv=fdatasync 2>&1 | grep bytes | awk '{print $(NF-1)$(NF)}')
+sync
+if [ "${machine}" = "Mac" ]
+then
+  output=$({ time -p sh -c "dd if=/dev/zero of="$file" bs=$bs count=$count 2>&1 && sync";} 2>&1)
+  bytes=$(echo $output | awk '{print $7}')
+  time=$(echo $output | awk '{print $16}')
+  write=$(echo "scale=2; $bytes / 1000 / 1000 / $time;" | bc -l)
+  write=$(echo $write MB/s)
+elif [ "${machine}" = "Linux" ]
+then
+  write=$(dd if=/dev/zero of=$file bs=$bs count=$count conv=fdatasync 2>&1 | grep bytes | awk '{print $(NF-1)$(NF)}')
+fi
+sync
 
 # Clean memory
-# If we don't do this, then read will be done from cache
+# If we don't do this, then read will be done from cache and we don't want
+# this of course
 echo "Cleaning memory (needs sudo)..."
-clean_mem
+if [ "${machine}" = "Mac" ]
+then
+  sudo purge
+elif [ "${machine}" = "Linux" ]
+then
+  sudo echo 3 > /proc/sys/vm/clean_mems
+fi
 
 # Read test
 echo "Running read test..."
-read=$($ddcmd if="$path"/.hddtstfile of=/dev/null bs=$bs count=$count conv=fdatasync 2>&1 | grep bytes | awk '{print $(NF-1)$(NF)}')
+if [ "${machine}" = "Mac" ]
+then
+  output=$({ time -p sh -c "dd if=$file of=/dev/null bs=$bs count=$count 2>&1 && sync";} 2>&1)
+  bytes=$(echo $output | awk '{print $7}')
+  time=$(echo $output | awk '{print $16}')
+  read=$(echo "scale=2; $bytes / 1000 / 1000 / $time;" | bc -l)
+  read=$(echo $read MB/s)
+elif [ "${machine}" = "Linux" ]
+then
+  read=$(dd if=$file of=/dev/null bs=$bs count=$count conv=fdatasync 2>&1 | grep bytes | awk '{print $(NF-1)$(NF)}')
+fi
 
 # Cleanup
 echo "Cleaning up..."
-clean_mem
-rm "$path"/.hddtstfile
+rm $file
 
 # Report
 echo "-------------------------"
