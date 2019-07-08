6ea81b2a9addd09bd57709cb7f2cf0fec40035b7
Ulrich Thiel <ulrich.thiel@sydney.edu.au>
2019-07-08 17:17:42 +0200

    Modified HDD script to use coreutils gdd under macOS

--- hdd.sh
+++ hdd.sh
@@ -3,15 +3,14 @@
 #
 # Determine HDD write/read speed under Linux and macOS.
 #
-# You will have to enter a path for the test file to be written to. This can
-# also be passed as argument to the script.
-#
-# This script uses dd. The macOS native dd has slightly different output and we
-# have to take care of this.
-#
 # Ulrich Thiel, 2019
 # ulthiel.com/math
 # mail@ulthiel.com
+#
+# You will have to enter a path for the test file to be written to. This can
+# also be passed as argument to the script.
+#
+# This script uses dd.
 #
 ################################################################################
 
@@ -33,7 +32,7 @@ else
 fi
 
 ################################################################################
-# Get OS (Linux and Mac behave a bit differently)
+# Get OS
 ################################################################################
 unameOut="$(uname -s)"
 case "${unameOut}" in
@@ -44,77 +43,44 @@ case "${unameOut}" in
     *)          machine="UNKNOWN:${unameOut}"
 esac
 
+# dd command
+if [ "${machine}" = "Mac" ]
+then
+  dd="gdd"
+else
+  dd="dd"
+fi
+
 ################################################################################
-# Function to clean memory
+# Function to flush and clean caches
 ################################################################################
-clean_mem () {
+flush () {
   sync
-  echo "Cleaning memory (needs sudo)..."
+  echo "Flushing caches (needs sudo)..."
   if [ "${machine}" = "Mac" ]
   then
     sudo purge
-  elif [ "${machine}" = "Linux" ]
-  then
-    sudo echo 3 > /proc/sys/vm/clean_mems
+  else
+    sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
   fi
 }
 
 ################################################################################
 # Main program
 ################################################################################
-
-# on macOS dd needs lower case block size; on Linux upper case...
-if [ "${machine}" = "Mac" ]
-then
-  bs=`echo "$bs" | tr '[:upper:]' '[:lower:]'`
-  count=`echo "$count" | tr '[:upper:]' '[:lower:]'`
-elif [ "${machine}" = "Linux" ]
-then
-  bs=`echo "$bs" | tr '[:lower:]' '[:upper:]'`
-  count=`echo "$count" | tr '[:lower:]' '[:upper:]'`
-fi
-
 file="$path/.hddtstfile"
 
-# Write test
-# The conv=fdatasync seems ensures that all data is really written before dd
-# quits and reports speed
+flush
+
 echo "Running write test..."
-sync
-if [ "${machine}" = "Mac" ]
-then
-  output=$({ time -p sh -c "dd if=/dev/zero of="$file" bs=$bs count=$count 2>&1 && sync";} 2>&1)
-  bytes=$(echo $output | awk '{print $7}')
-  time=$(echo $output | awk '{print $16}')
-  write=$(echo "scale=2; $bytes / 1000 / 1000 / $time;" | bc -l)
-  write=$(echo $write MB/s)
-elif [ "${machine}" = "Linux" ]
-then
-  write=$(dd if=/dev/zero of=$file bs=$bs count=$count conv=fdatasync 2>&1 | grep bytes | awk '{print $(NF-1)$(NF)}')
-fi
+write=$($dd if=/dev/zero of=$file bs=$bs count=$count conv=fdatasync 2>&1 | grep bytes | awk '{print $(NF-1)$(NF)}')
 
-# Clean memory
-# If we don't do this, then read will be done from cache and we don't want
-# this of course
-clean_mem
+flush
 
-# Read test
 echo "Running read test..."
-if [ "${machine}" = "Mac" ]
-then
-  output=$({ time -p sh -c "dd if=$file of=/dev/null bs=$bs count=$count 2>&1 && sync";} 2>&1)
-  bytes=$(echo $output | awk '{print $7}')
-  time=$(echo $output | awk '{print $16}')
-  read=$(echo "scale=2; $bytes / 1000 / 1000 / $time;" | bc -l)
-  read=$(echo $read MB/s)
-elif [ "${machine}" = "Linux" ]
-then
-  read=$(dd if=$file of=/dev/null bs=$bs count=$count conv=fdatasync 2>&1 | grep bytes | awk '{print $(NF-1)$(NF)}')
-fi
+read=$($dd if=$file of=/dev/null bs=$bs count=$count conv=fdatasync 2>&1 | grep bytes | awk '{print $(NF-1)$(NF)}')
 
-# Cleanup
-echo "Cleaning up..."
-clean_mem
+flush
 rm $file
 
 # Report
